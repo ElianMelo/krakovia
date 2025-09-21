c3fcacb96999c53c8662e772158fbfed7c33b39f
Elian Melo <elianmelomorais@gmail.com>
2025-09-20 22:52:54 -0300

    Ajust projectile despawn

--- Assets/Assets/Scripts/Player/PlayerProjectile.cs
+++ Assets/Assets/Scripts/Player/PlayerProjectile.cs
@@ -1,10 +1,27 @@
+using System.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerProjectile : MonoBehaviour
+public class PlayerProjectile : NetworkBehaviour
 {
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsHost)
+        {
+            StartCoroutine(destroySelf());
+        }
+    }
+
+    private IEnumerator destroySelf()
+    {
+        yield return new WaitForSeconds(3f);
+        NetworkObject.DontDestroyWithOwner = true;
+        NetworkObject.Despawn();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger?");
         EnemyController enemyController = other.gameObject.GetComponent<EnemyController>();
         if(enemyController != null)
         {
